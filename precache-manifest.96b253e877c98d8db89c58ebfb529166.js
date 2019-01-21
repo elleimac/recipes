fb2db77d5d44a05fe9eb55639e306914d1712f9f
elleimac <elleimac@users.noreply.github.com>
2019-01-21 11:37:15 -0800

    Updates

---
+++ precache-manifest.96b253e877c98d8db89c58ebfb529166.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "204d141c6f5f5c2bbee018228875cedc",
+    "url": "/recipes/static/media/lemons.204d141c.jpg"
+  },
+  {
+    "revision": "aa6e30bfc1d295068424",
+    "url": "/recipes/static/js/runtime~main.aa6e30bf.js"
+  },
+  {
+    "revision": "367e150470c7e526b89c",
+    "url": "/recipes/static/js/main.367e1504.chunk.js"
+  },
+  {
+    "revision": "004f5975821e397aac35",
+    "url": "/recipes/static/js/1.004f5975.chunk.js"
+  },
+  {
+    "revision": "367e150470c7e526b89c",
+    "url": "/recipes/static/css/main.153877fb.chunk.css"
+  },
+  {
+    "revision": "004f5975821e397aac35",
+    "url": "/recipes/static/css/1.052c257e.chunk.css"
+  },
+  {
+    "revision": "5246c0ce5c465e6cd8a30781cd91ca69",
+    "url": "/recipes/index.html"
+  }
+];
